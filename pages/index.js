2c808740ba49714b1ff0a7056de32044bb075395
Topmewan <anon8733@gmail.com>
2022-01-11 18:34:43 +0500

    create api func

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = () => {
 
       <Banner
         purpose='Покупка дома'
-        title1='Покупка домой для '
+        title1='Покупка дома для'
         title2='всех'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
         desc1='Апартаменты,Виллы,Дома'
---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+export const fetchApi = async (url) => {
+  const {data} =  await axios.get((url),{
+    headers: {
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': '46aac9013bmsh64f11330b04302ap1d6206jsn1b776264e118'
+    }
+  })
+}
